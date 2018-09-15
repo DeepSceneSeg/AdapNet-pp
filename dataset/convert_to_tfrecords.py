9fa6f5df39473c9674fece39fa44002718efaedc
Rohit Mohan <rohitmohan1914@gmail.com>
2018-09-15 17:17:15 +0200

    update spaces

--- dataset/convert_to_tfrecords.py
+++ dataset/convert_to_tfrecords.py
@@ -1,25 +1,21 @@
-''' AdapNet++:  Self-Supervised Model Adaptation for Multimodal Semantic Segmentation
-
- Copyright (C) 2018  Abhinav Valada, Rohit Mohan and Wolfram Burgard
-
+''' AdapNet:  Adaptive  Semantic  Segmentation
+              in  Adverse  Environmental  Conditions
+ Copyright (C) 2018  Abhinav Valada, Johan Vertens , Ankit Dhall and Wolfram Burgard
  This program is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
  (at your option) any later version.
-
  This program is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
-
  You should have received a copy of the GNU General Public License
  along with this program.  If not, see <http://www.gnu.org/licenses/>.'''
 
-
-
-import tensorflow as tf
 import argparse
 import cv2
+import numpy as np
+import tensorflow as tf
 
 def _int64_feature(data):
     return tf.train.Feature(int64_list=tf.train.Int64List(value=[data]))
@@ -27,62 +23,72 @@ def _int64_feature(data):
 def _bytes_feature(data):
     return tf.train.Feature(bytes_list=tf.train.BytesList(value=[data]))
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-f','--file')
-parser.add_argument('-r','--record')
+PARSER = argparse.ArgumentParser()
+PARSER.add_argument('-f', '--file')
+PARSER.add_argument('-r', '--record')
+PARSER.add_argument('-m', '--mean')
 
 def decode(txt):
-    with open(txt) as f:
-         all_list=f.readlines()
-    file_list=[]
+    with open(txt) as file_handler:
+        all_list = file_handler.readlines()
+
+    file_list = []
     for line in all_list:
-        temp=line.split(' ')
+        temp = line.split(' ')
         file_list.append(temp)
+
     return file_list
-     
-def convert(f,record_name):
-    count = 0
-    writer=tf.python_io.TFRecordWriter(record_name)
+
+def convert(f, record_name, mean_flag):
+    count = 0.0
+    writer = tf.python_io.TFRecordWriter(record_name)
+
+    if mean_flag:
+        mean = np.zeros(cv2.imread(f[0][0]).shape, np.float32)
+
     for name in f:
         modality1 = cv2.imread(name[0])
-        label     = cv2.imread(name[1][:-1],cv2.IMREAD_GRAYSCALE)  
-        height    = modality1.shape[0]
-        width     = modality1.shape[1]
-        modality1 = modality1.tostring() 
-        modality2 = modality2.tostring() 
-        label     = label.tostring()
-        features={'height':_int64_feature(height),
-                  'width':_int64_feature(width),
-                  'modality1':_bytes_feature(modality1),
-                  'label':_bytes_feature(label),
-                 }
-        example=tf.train.Example(features=tf.train.Features(feature=feature))
+        if mean_flag:
+            mean += modality1
+        
+        label = cv2.imread(name[1], cv2.IMREAD_GRAYSCALE)
+        height = modality1.shape[0]
+        width = modality1.shape[1]
+        modality1 = modality1.tostring()
+        label = label.tostring()
+        features = {'height':_int64_feature(height),
+                    'width':_int64_feature(width),
+                    'modality1':_bytes_feature(modality1),
+                    'label':_bytes_feature(label),
+                   }
+        example = tf.train.Example(features=tf.train.Features(feature=features))
         writer.write(example.SerializeToString())
-        if (count+1)%1==0:
-           print 'Processed data: {}'.format(count)
-           sys.stdout.flush()    
-        count=count+1
 
+        if (count+1)%1 == 0:
+            print 'Processed data: {}'.format(count)
 
+        count = count+1
 
-
-
-
-
+    if mean_flag:
+        mean = mean/count
+        np.save(record_name.split('.')[0]+'.npy', mean)
 
 def main():
-    args = parser.parse_args()
+    args = PARSER.parse_args()
     if args.file:
-        file_list=decode(args.file)
+        file_list = decode(args.file)
     else:
         print '--file file_address missing'
         return
     if args.record:
-        record_name=args.record
+        record_name = args.record
     else:
         print '--record tfrecord name missing'
         return
-    convert(file_list,record_name)
+    mean_flag = False
+    if args.record:
+        mean_flag = args.mean
+    convert(file_list, record_name, mean_flag)
 
-if __name__=='__main__':
-        main()
+if __name__ == '__main__':
+    main()
--- evaluate.py
+++ evaluate.py
@@ -15,76 +15,71 @@
  You should have received a copy of the GNU General Public License
  along with this program.  If not, see <http://www.gnu.org/licenses/>.'''
 
-
-
-import tensorflow as tf
-from dataset.helper import *
-import numpy as np
 import argparse
-import os
 import datetime
 import importlib
+import os
+import numpy as np
+import tensorflow as tf
 import yaml
-import re
-import pickle
-parser = argparse.ArgumentParser()
-parser.add_argument('-c','--config')
+from dataset.helper import *
+
+PARSER = argparse.ArgumentParser()
+PARSER.add_argument('-c', '--config', default='config/cityscapes_test.config')
 
 def test_func(config):
     os.environ['CUDA_VISIBLE_DEVICES'] = config['gpu_id']
-    module = importlib.import_module('models.'+config['model'])
-    model_func = getattr(module,config['model'])     
+    module = importlib.import_module('models.' + config['model'])
+    model_func = getattr(module, config['model'])
     data_list, iterator = get_test_data(config)
-    resnet_name='resnet_v1_50'
+    resnet_name = 'resnet_v1_50'
+
     with tf.variable_scope(resnet_name):
-    	model = model_func(num_classes=config['num_classes'],training=False)
-        images_pl = tf.placeholder(tf.float32, [None, config['height'],config['width'], 3])
-        logits = model.build_graph(images_pl)
-    config1 =  tf.ConfigProto()
+        model = model_func(num_classes=config['num_classes'], training=False)
+        images_pl = tf.placeholder(tf.float32, [None, config['height'], config['width'], 3])
+        model.build_graph(images_pl)
+
+    config1 = tf.ConfigProto()
     config1.gpu_options.allow_growth = True
-    sess=tf.Session(config=config1)
+    sess = tf.Session(config=config1)
     sess.run(tf.global_variables_initializer())
     import_variables = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)
-    print 'total_variables_loaded:',len(import_variables)  
-   
+    print 'total_variables_loaded:', len(import_variables)
     saver = tf.train.Saver(import_variables)
     saver.restore(sess, config['checkpoint'])
     sess.run(iterator.initializer)
-    step=0
-    total_num=0
-    output_matrix=np.zeros([config['num_classes'],3])
+    step = 0
+    total_num = 0
+    output_matrix = np.zeros([config['num_classes'], 3])
     while 1:
-    	try:
-            img,label = sess.run([data_list[0],data_list[1]])
-           
-            feed_dict={images_pl : img }
-            probabilities = sess.run([model.softmax],feed_dict=feed_dict)
-            prediction=np.argmax(probabilities[0],3)
-            gt=np.argmax(label,3)
-            prediction[gt==0]=0
-            output_matrix=compute_output_matrix(gt,prediction,output_matrix)
+        try:
+            img, label = sess.run([data_list[0], data_list[1]])
+            feed_dict = {images_pl : img}
+            probabilities = sess.run([model.softmax], feed_dict=feed_dict)
+            prediction = np.argmax(probabilities[0], 3)
+            gt = np.argmax(label, 3)
+            prediction[gt == 0] = 0
+            output_matrix = compute_output_matrix(gt, prediction, output_matrix)
             total_num += label.shape[0]
             if (step+1) % config['skip_step'] == 0:
-               print '%s %s] %d. iou updating' \
+                print '%s %s] %d. iou updating' \
                   % (str(datetime.datetime.now()), str(os.getpid()), total_num)
-               print 'mIoU: ',compute_iou(output_matrix)
-            #compute_iou(confusion_matrix)
+                print 'mIoU: ', compute_iou(output_matrix)
+
             step += 1
-            #print 'total_data: ',total_num
-        except tf.errors.OutOfRangeError: 
-            print 'mIoU: ',compute_iou(output_matrix),'total_data: ',total_num
+
+        except tf.errors.OutOfRangeError:
+            print 'mIoU: ', compute_iou(output_matrix), 'total_data: ', total_num
             break
 
-
-
 def main():
-    args = parser.parse_args()
+    args = PARSER.parse_args()
     if args.config:
-        f=open(args.config)
-        config=yaml.load(f)
+        file_address = open(args.config)
+        config = yaml.load(file_address)
     else:
         print '--config config_file_address missing'
     test_func(config)
 
-if __name__=='__main__':
-        main()
+if __name__ == '__main__':
+    main()
