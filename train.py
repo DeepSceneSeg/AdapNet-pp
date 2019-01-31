d4ef601cbb654fe639a2ae9d1cc24ddd77f728f9
Rohit Mohan <rohitmohan1914@gmail.com>
2019-01-31 14:37:05 +0100

    Update train.py

--- train.py
+++ train.py
@@ -60,20 +60,21 @@ def train_func(config):
         print 'Model Loaded'
 
     else:
-        reader = tf.train.NewCheckpointReader(config['intialize'])
-        var_str = reader.debug_string()
-        name_var = re.findall('[A-Za-z0-9/:_]+ ', var_str)
-        import_variables = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)
-        initialize_variables = {} 
-        for var in import_variables: 
-            if var.name+' ' in  name_var:
-                initialize_variables[var.name] = var
+        if 'intialize' in config:
+            reader = tf.train.NewCheckpointReader(config['intialize'])
+            var_str = reader.debug_string()
+            name_var = re.findall('[A-Za-z0-9/:_]+ ', var_str)
+            import_variables = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)
+            initialize_variables = {} 
+            for var in import_variables: 
+                if var.name+' ' in  name_var:
+                    initialize_variables[var.name] = var
 
-        saver = tf.train.Saver(initialize_variables)
-        saver.restore(save_path=config['intialize'], sess=sess)
+            saver = tf.train.Saver(initialize_variables)
+            saver.restore(save_path=config['intialize'], sess=sess)
+            print 'Pretrained Intialization'
         saver = tf.train.Saver(max_to_keep=1000)
-        print 'Intialized'
-    
+       
     while 1:
         try:
             img, label = sess.run([data_list[0], data_list[1]])
